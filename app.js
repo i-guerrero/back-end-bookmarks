2fa38f9625a166bb3b9c1bc6abfa5bd909f2146a
RazEfron <razefron01@gmail.com>
2023-01-18 18:14:29 -0500

    reorder routes in app.js

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ const express = require("express");
 // CONFIGURATION
 const app = express();
 
-// Bookmarks ROUTES
-const bookmarksController = require("./controllers/bookmarksController");
-app.use("/bookmarks", bookmarksController);
-
-// 404 PAGE
-app.get("*", (req, res) => {
-  res.status(404).send("Page Not Found");
-});
-
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
@@ -21,6 +12,15 @@ app.use(express.json());
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to Bookmarks App");
+});
+
+// Bookmarks ROUTES
+const bookmarksController = require("./controllers/bookmarksController");
+app.use("/bookmarks", bookmarksController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found");
 });
 
 // EXPORT
